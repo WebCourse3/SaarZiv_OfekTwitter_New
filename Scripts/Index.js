3ffca9e38f2ce0860d057c1aea067b6d8fa69a52
Saar Ziv <saarzivon@gmail.com>
2017-10-26 00:06:23 +0300

    After a bit js at home

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -1,3 +1,12 @@
 function Publish() {
-	alert("hey")
+    var Tweet = document.getElementById('Tweet').value;
+	document.getElementById('Tweet').value="";
+
+	var node  = document.createElement("LI");
+	var textNode = document.createTextNode(Tweet);
+    var currentTweet = node.appendChild(textNode);
+
+	document.getElementById('test').appendChild(currentTweet);
+
+
 }
