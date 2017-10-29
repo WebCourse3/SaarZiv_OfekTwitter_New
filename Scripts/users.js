14470944798ef5ab7130181e3813c41c470ee7b5
Jbt <saarzivon@gmail.com>
2017-10-29 14:23:41 +0200

    JS drill really ready for SAKAK

--- Scripts/users.js
+++ Scripts/users.js
@@ -1,14 +1,15 @@
 var usersList = [
-	{"username":"dani","isFollowing":false},
+	{"username":"dani","isFollowing":true},
 	{"username":"moshe","isFollowing":true},
 	{"username":"esti","isFollowing":true},
 	{"username":"mayan","isFollowing":true},
+	{"username":"rami","isFollowing":true},
 	{"username":"rami","isFollowing":false},
 	{"username":"baruch","isFollowing":false},
 	{"username":"nesi","isFollowing":false},
 	{"username":"varum","isFollowing":true},
-	{"username":"moko","isFollowing":false},
-	{"username":"dani avshalomov ","isFollowing":false},
+	{"username":"moko","isFollowing":true},
+	{"username":"dani","isFollowing":false},
 	{"username":"loko","isFollowing":false},
 	{"username":"bar","isFollowing":false},
 	{"username":"eli","isFollowing":true},
@@ -21,18 +22,18 @@ function loadUsers() {
 	var loadFollowersFragment = document.createDocumentFragment();
 	var loadFolloweesFragment = document.createDocumentFragment();
 	for (var i in usersList){
-        var divPanelBody
+        var divPanelBody;
 
         if(usersList[i].isFollowing){
-            divPanelBody =  createPanelBody(usersList[i].username,true);
-			var divPanel = createDivPanel(divPanelBody);
-			loadFolloweesFragment.appendChild(divPanel)
-		}
+	        divPanelBody =  createPanelBody(usersList[i].username,true);
+	        var divPanel = createDivPanel(divPanelBody,true);
+	        loadFolloweesFragment.appendChild(divPanel);
+
+        }
 		else{
-            divPanelBody =  createPanelBody(usersList[i].username,false);
-			var divPanel = createDivPanel(divPanelBody);
-			var divCol = createDivCol(divPanel);
-			loadFollowersFragment.appendChild(divCol);
+	        divPanelBody =  createPanelBody(usersList[i].username,false);
+	        var divPanel = createDivPanel(divPanelBody,false);
+	        loadFollowersFragment.appendChild(divPanel);
 		}
 	}
 	LoadFollowers(loadFollowersFragment);
@@ -40,24 +41,17 @@ function loadUsers() {
 }
 
 function LoadFollowers(loadFollowersFragment) {
-	var followersDiv = document.getElementById("js-followers");
-	followersDiv.appendChild(loadFollowersFragment);
+	var jsNotFollowingElement = document.getElementById("js-notFollowing");
+	jsNotFollowingElement.appendChild(loadFollowersFragment);
 }
 function LoadFollowees(loadFolloweesFragment) {
 	var followeesDiv = document.getElementById("js-followees-thumbnail");
 	followeesDiv.appendChild(loadFolloweesFragment);
 }
 
-function createDivCol(divPanel){
-	var divCol2Xs = document.createElement("div");
-	divCol2Xs.className = "col-sm-2";
-	divCol2Xs.appendChild(divPanel);
-	return divCol2Xs;
-
-}
-function createDivPanel(divPanelBody) {
+function createDivPanel(divPanelBody,isFollowing) {
 	var divPanel = document.createElement("div");
-	divPanel.className = "panel panel-default ";
+	(isFollowing) ? divPanel.className = "panel panel-default" : divPanel.className = "panel panel-default col-xs-2" ;
 	divPanel.appendChild(divPanelBody);
 	return divPanel;
 }
@@ -72,7 +66,7 @@ function createPanelBody(username,bool) {
 	img.src = "../Images/useravatar.png";
 	followButton.type = "button";
     (bool) ? followButton.style.marginLeft = "0" : followButton.style.marginLeft = "-10%";
-	(bool) ? followButton.innerHTML = "Follow" : followButton.innerHTML = "Unfollow";
+	(bool) ? followButton.innerHTML = "Unfollow" : followButton.innerHTML = "Follow";
 	(followButton.innerHTML === "Follow") ? followButton.className = "btn btn-primary" : followButton.className = "btn btn-info";
 	followButton.onclick = function() {userBtnClick(this)};
 	userNameP.innerHTML = username;
@@ -88,35 +82,31 @@ function userBtnClick(btnElement) {
 	var panelBodyElement = btnElement.parentElement;
     var panelElement = panelBodyElement.parentElement;
     var jsFolloweesElement  =  document.getElementById('js-followees-thumbnail');
-    var jsFollowersElement  =  document.getElementById('js-followers');
+    var jsNotFollowingElement  =  document.getElementById('js-notFollowing');
     if(btnElement.innerHTML === "Follow") {
-        followUser(btnElement,panelElement,jsFolloweesElement,jsFollowersElement);
+        followUser(btnElement,panelElement,jsFolloweesElement);
 	}
 	else {
-        unFollowUser(btnElement,panelElement,jsFolloweesElement,jsFollowersElement);
+        unFollowUser(btnElement,panelElement,jsNotFollowingElement);
     }
 }
-function followUser(btnElement,panelElement,jsFolloweesElement,jsFollowersElement) {
+function followUser(btnElement,panelElement,jsFolloweesElement) {
     btnElement.innerHTML = "Unfollow";
     btnElement.className = "btn btn-info";
-    jsFolloweesElement.removeChild(panelElement);
-    var newColSm2Element = document.createElement('div');
-    newColSm2Element.className = "col-sm-2";
-    newColSm2Element.appendChild(panelElement);
-    jsFollowersElement.appendChild(newColSm2Element);
+    panelElement.className = "panel panel-default";
+    jsFolloweesElement.appendChild(panelElement);
 }
-function unFollowUser(btnElement,panelElement,jsFolloweesElement,jsFollowersElement){
+function unFollowUser(btnElement,panelElement,jsNotFollowingElement){
     btnElement.innerHTML = "Follow";
     btnElement.className = "btn btn-primary";
-    var colSm2Element = panelElement.parentElement;
-    jsFollowersElement.removeChild(colSm2Element);
-    jsFolloweesElement.appendChild(panelElement);
+	panelElement.className = "panel panel-default col-xs-2";
+	jsNotFollowingElement.appendChild(panelElement);
 }
 
 function filterUsers(inputElement) {
 	var users = document.getElementsByClassName("js-userName");
-	var usersToHideArr = new Array() ;
-	var usersToShowArr = new Array() ;
+	var usersToHideArr = [];
+	var usersToShowArr = [];
 	for (var i  in users){
 		var userText = users[i].innerHTML;
 		if(userText !== undefined){
@@ -133,35 +123,24 @@ function filterUsers(inputElement) {
 }
 function hideOtherUsers(usersToHideArr) {
     var jsFolloweesElement  =  document.getElementById('js-followees-thumbnail');
-    var jsFollowersElement  =  document.getElementById('js-followers');
+    var jsNotFollowingElement  =  document.getElementById('js-notFollowing');
     for(var i in usersToHideArr){
     	var panelBody  = usersToHideArr[i].parentElement;
     	var panel = panelBody.parentElement;
-    	if(panel.parentElement === jsFolloweesElement) {
+    	if(panel.parentElement === jsFolloweesElement || panel.parentElement === jsNotFollowingElement) {
             panel.style.display = "none";
         }
-        else {
-    		var col = panel.parentElement;
-    		if(col.parentElement === jsFollowersElement){
-    			col.style.display = "none";
-			}
-		}
+
 	}
 }
 function showOtherUsers(usersToShowArr) {
 	var jsFolloweesElement  =  document.getElementById('js-followees-thumbnail');
-	var jsFollowersElement  =  document.getElementById('js-followers');
+	var jsNotFollowingElement  =  document.getElementById('js-notFollowing');
 	for(var i in usersToShowArr){
 		var panelBody  = usersToShowArr[i].parentElement;
 		var panel = panelBody.parentElement;
-		if(panel.parentElement === jsFolloweesElement) {
+		if(panel.parentElement === jsFolloweesElement || panel.parentElement === jsNotFollowingElement) {
 			panel.style.display = "";
-		}
-		else {
-			var col = panel.parentElement;
-			if(col.parentElement === jsFollowersElement){
-				col.style.display = "";
-			}
 		}
 	}
 }
